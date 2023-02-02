ec91d17e5b50b22ccddd2691601337e8bcacfd17
samuug <113547223+samuug@users.noreply.github.com>
2023-02-02 18:44:45 +0000

    hemos creado las preguntas

---
+++ pregunta1.java
@@ -0,0 +1,17 @@
+//¿Qué algoritmos usas en tu vida cotidiana? ¿Piensas que podrías escribir un programa para hacerlos más eficientes?
+
+
+
+// Path: pregunta2.java
+
+//¿Qué algoritmos crees que usan por tus juegos y aplicaciones favoritas?
+
+
+
+
+//Path: pregunta3.java
+
+//¿Haz hecho alguna vez un algoritmo para un programa? ¿Qué hacía? ¿Era correcto y eficiente?
+
+
+
